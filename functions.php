bac3e4dc6651c2cd4effb7beaac32d679684a88c
Mishari Muqbil <mishari@mishari.net>
2015-07-16 15:05:10 +0000

    CSS in functions reenabled

--- functions.php
+++ functions.php
@@ -8,19 +8,19 @@ require_once(STYLESHEETPATH . '/inc/collection.php');
 
 function mekongeye_styles() {
 
-	// $options = get_option('mekongeye_options');
-	//
-	// $css_base = get_stylesheet_directory_uri() . '/assets/css/';
-	//
-	//
-	// wp_register_style('webfont-sans-pro', '//fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700,400italic,600italic,700italic');
-	// wp_register_style('webfont-crimson-text', '//fonts.googleapis.com/css?family=Crimson+Text:400,400italic,700,700italic');
-	// wp_register_style('mekongeye-base',  $css_base . 'main.css', array('webfont-sans-pro', 'webfont-crimson-text'));
-	//
-	// wp_enqueue_style('mekongeye-base');
-	// if($options['style']) {
-	// 	wp_enqueue_style('mekongeye-' . $options['style']);
-	// }
+	$options = get_option('mekongeye_options');
+
+	$css_base = get_stylesheet_directory_uri() . '/assets/css/';
+
+
+	wp_register_style('webfont-sans-pro', '//fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700,400italic,600italic,700italic');
+	wp_register_style('webfont-crimson-text', '//fonts.googleapis.com/css?family=Crimson+Text:400,400italic,700,700italic');
+	wp_register_style('mekongeye-base',  $css_base . 'main.css', array('webfont-sans-pro', 'webfont-crimson-text'));
+
+	wp_enqueue_style('mekongeye-base');
+	if($options['style']) {
+		wp_enqueue_style('mekongeye-' . $options['style']);
+	}
 	
 	wp_dequeue_style('jeo-main');
 
