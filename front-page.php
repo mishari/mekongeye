3dae248ec732eed94204fd3ebe88b3213e4b26aa
BabyThor <w.wangtrakoon@gmail.com>
2015-08-06 19:19:48 +0700

    update front page to get newest 10 posts for newStream section

--- front-page.php
+++ front-page.php
@@ -11,164 +11,47 @@ get_header(); ?>
     <?php endwhile; ?>
     </div>
     <div id="stories-right">
-        <!-- begin story container for recent stories -->
         <section class="sc-container" id="recent">
-
             <h2 class="alt">NewsStream</h2>
-
-            <!-- begin slice -->
             <div class="sc-slice size-xs">
-
-                <article class="sc-story ">
-                    <a href="#">
-                        <div class="sc-story__bd">
-                            <h4>If there is no image, show only the text headine for the story <i class="dateline">Publication Name</i> <img src="images/arrow-new-site.png" alt=""></h4>
-                        </div>
-                    </a>
-                </article>
-
+            <?php 
+            $args = array(
+                'posts_per_page'   => 10,
+                'orderby'          => 'post_date',
+                'order'            => 'DESC',
+                'post_type'        => array('post', 'link', 'sequence', 'map'),
+                'post_status'      => 'publish',
+                'suppress_filters' => true
+            );
+            $posts = get_posts( $args );
+            foreach ( $posts as $post ) {
+                $author_first_name = get_the_author_meta( 'first_name', $post->post_author );
+                $author_last_name = get_the_author_meta( 'last_name', $post->post_author );
+            ?>
                 <article class="sc-story">
-                    <a href="#">
+                    <?php 
+                    if ($post->post_type == 'link') {
+                        $link = get_post_meta($post->ID, 'link_target', true);
+                        echo '<a href="' . $link .'">';
+                    }
+                    else {
+                        echo '<a href="' . $post->guid .'">';
+                    }
+                    ?>
                         <div class="sc-story__bd">
-                            <h4>Small story hed goes here spot for story <i class="dateline">Publication Name</i> <img src="images/arrow-new-site.png" alt=""></h4>
+                            <h4><?php echo $post->post_title; ?><i class="dateline"> <?php echo $author_first_name . ' ' . $author_last_name; ?></i></h4>
                         </div>
                     </a>
                 </article>
-
-                <article class="sc-story">
-                    <a href="#">
-                        <div class="sc-story__bd">
-                            <h4><b class="kicker">Eye Opinion</b> Small story hed goes here spot for story </h4>
-                        </div>
-                    </a>
-                </article>
-
-                <article class="sc-story ">
-                    <a href="#">
-                        <div class="sc-story__bd">
-                            <h4>If there is no image, show only the text headine for the story <i class="dateline">Publication Name</i> <img src="images/arrow-new-site.png" alt=""></h4>
-                        </div>
-                    </a>
-                </article>
-
-                <article class="sc-story ">
-                    <a href="#">
-                        <div class="sc-story__bd">
-                            <h4>If there is no image, show only the text headine for the story <i class="dateline">Publication Name</i> <img src="images/arrow-new-site.png" alt=""></h4>
-                        </div>
-                    </a>
-                </article>
-
-                <article class="sc-story">
-                    <a href="#">
-                        <div class="sc-story__bd">
-                            <h4>Small story hed goes here spot for story <i class="dateline">Publication Name</i> <img src="images/arrow-new-site.png" alt=""></h4>
-                        </div>
-                    </a>
-                </article>
-
-                <article class="sc-story">
-                    <a href="#">
-                        <div class="sc-story__bd">
-                            <h4><b class="kicker">Eye Original</b> Small story hed goes here spot for story </h4>
-                        </div>
-                    </a>
-                </article>
-
-                <article class="sc-story ">
-                    <a href="#">
-                        <div class="sc-story__bd">
-                            <h4>If there is no image, show only the text headine for the story <i class="dateline">Publication Name</i> <img src="images/arrow-new-site.png" alt=""></h4>
-                        </div>
-                    </a>
-                </article>
-
-                <article class="sc-story">
-                    <a href="#">
-                        <div class="sc-story__bd">
-                            <h4>Small story hed goes here spot for story <i class="dateline">Publication Name</i> <img src="images/arrow-new-site.png" alt=""></h4>
-                        </div>
-                    </a>
-                </article>
-
-                <article class="sc-story">
-                    <a href="#">
-                        <div class="sc-story__bd">
-                            <h4><b class="kicker">Eye Original</b> Small story hed goes here spot for story </h4>
-                        </div>
-                    </a>
-                </article>
-
-                <article class="sc-story ">
-                    <a href="#">
-                        <div class="sc-story__bd">
-                            <h4>If there is no image, show only the text headine for the story <i class="dateline">Publication Name</i> <img src="images/arrow-new-site.png" alt=""></h4>
-                        </div>
-                    </a>
-                </article>
-
-                <article class="sc-story">
-                    <a href="#">
-                        <div class="sc-story__bd">
-                            <h4>Small story hed goes here spot for story <i class="dateline">Publication Name</i> <img src="images/arrow-new-site.png" alt=""></h4>
-                        </div>
-                    </a>
-                </article>
-
-                <article class="sc-story">
-                    <a href="#">
-                        <div class="sc-story__bd">
-                            <h4><b class="kicker">Eye Opinion</b> Small story hed goes here spot for story</h4>
-                        </div>
-                    </a>
-                </article>
-
-                <article class="sc-story ">
-                    <a href="#">
-                        <div class="sc-story__bd">
-                            <h4>If there is no image, show only the text headine for the story <i class="dateline">Publication Name</i> <img src="images/arrow-new-site.png" alt=""></h4>
-                        </div>
-                    </a>
-                </article>
-
-                <article class="sc-story">
-                    <a href="#">
-                        <div class="sc-story__bd">
-                            <h4>Small story hed goes here spot for story <i class="dateline">Publication Name</i> <img src="images/arrow-new-site.png" alt=""></h4>
-                        </div>
-                    </a>
-                </article>
-
-                <article class="sc-story">
-                    <a href="#">
-                        <div class="sc-story__bd">
-                            <h4><b class="kicker">Eye Opinion</b> Small story hed goes here spot for story </h4>
-                        </div>
-                    </a>
-                </article>
-
-                <article class="sc-story ">
-                    <a href="#">
-                        <div class="sc-story__bd">
-                            <h4>Small story hed goes here spot for story <i class="dateline">Publication Name</i> <img src="images/arrow-new-site.png" alt=""></h4>
-                        </div>
-                    </a>
-                </article>
-
+            <?php 
+            }
+            ?>
             </div>
-            <!-- end slice -->
-
         </section>
-        <!-- end story container -->
 
-        <!-- begin story container for recent releases -->
         <section class="sc-container" id="">
-
             <h2 class="alt">Press Releases</h2>
-
-            <!-- begin slice -->
             <div class="sc-slice size-xs">
-
                 <article class="sc-story ">
                     <a href="#">
                         <div class="sc-story__bd">
@@ -176,139 +59,14 @@ get_header(); ?>
                         </div>
                     </a>
                 </article>
-
-                <article class="sc-story">
-                    <a href="#">
-                        <div class="sc-story__bd">
-                            <h4>Small story hed goes here spot for story <i class="dateline">25 May 2015</i></h4>
-                        </div>
-                    </a>
-                </article>
-
-                <article class="sc-story">
-                    <a href="#">
-                        <div class="sc-story__bd">
-                            <h4>Small story hed goes here spot for story <i class="dateline">25 May 2015</i></h4>
-                        </div>
-                    </a>
-                </article>
-
-                <article class="sc-story ">
-                    <a href="#">
-                        <div class="sc-story__bd">
-                            <h4>If there is no image, show only the text headine for the story <i class="dateline">25 May 2015</i></h4>
-                        </div>
-                    </a>
-                </article>
-
-                <article class="sc-story">
-                    <a href="#">
-                        <div class="sc-story__bd">
-                            <h4>Small story hed goes here spot for story <i class="dateline">25 May 2015</i></h4>
-                        </div>
-                    </a>
-                </article>
-
-
-                <article class="sc-story">
-                    <a href="#">
-                        <div class="sc-story__bd">
-                            <h4>Small story hed goes here spot for story <i class="dateline">25 May 2015</i></h4>
-                        </div>
-                    </a>
-                </article>
-
-                <article class="sc-story ">
-                    <a href="#">
-                        <div class="sc-story__bd">
-                            <h4>Small story hed goes here spot for story <i class="dateline">25 May 2015</i></h4>
-                        </div>
-                    </a>
-                </article>
-
             </div>
-            <!-- end slice -->
-
         </section>
-        <!-- end story container -->
-
-        <!-- begin story container -->
-        <section class="sc-container">
-
-            <h2 class="alt"><a href="#">Most Read <i>Further text here</i> <b>»</b></a></h2>
-
-            <!-- begin slice -->
-            <div class="sc-slice size-sm">
-
-                <article class="sc-story ">
-                    <a href="#">
-                        <div class="sc-story__bd">
-                            <h4>If there is no image, show only the text headine for the story <i class="dateline">25 May 2015</i></h4>
-                        </div>
-                    </a>
-                </article>
-
-                <article class="sc-story">
-                    <a href="#">
-                        <div class="sc-story__bd">
-                            <h4>Small story hed goes here spot for story <i class="dateline">25 May 2015</i></h4>
-                        </div>
-                    </a>
-                </article>
-
-                <article class="sc-story">
-                    <a href="#">
-                        <div class="sc-story__bd">
-                            <h4><b class="kicker">Kicker</b> Small story hed goes here spot for story <i class="dateline">25 May 2015</i></h4>
-                        </div>
-                    </a>
-                </article>
-
-                <article class="sc-story ">
-                    <a href="#">
-                        <div class="sc-story__bd">
-                            <h4>If there is no image, show only the text headine for the story <i class="dateline">25 May 2015</i></h4>
-                        </div>
-                    </a>
-                </article>
-
-                <article class="sc-story">
-                    <a href="#">
-                        <div class="sc-story__bd">
-                            <h4>Small story hed goes here spot for story <i class="dateline">25 May 2015</i></h4>
-                        </div>
-                    </a>
-                </article>
-
-
-                <article class="sc-story">
-                    <a href="#">
-                        <div class="sc-story__bd">
-                            <h4><b class="kicker">Opinion</b> Small story hed goes here spot for story <i class="dateline">25 May 2015</i></h4>
-                        </div>
-                    </a>
-                </article>
-
-                <article class="sc-story ">
-                    <a href="#">
-                        <div class="sc-story__bd">
-                            <h4>Small story hed goes here spot for story <i class="dateline">25 May 2015</i></h4>
-                        </div>
-                    </a>
-                </article>
-
-            </div>
-            <!-- end slice -->
-
-        </section>
-        <!-- end story container -->
 
         <div id="subscribe-right">
 
             <h3>Subscribe</h3>
 
         </div>
-
 
     </div>
 </div>
