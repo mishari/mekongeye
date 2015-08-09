f9431e19a50b709ea7933f21ad1bf36b8ed27a72
BabyThor <w.wangtrakoon@gmail.com>
2015-08-09 10:20:07 +0700

    add more section for right sidebar

--- front-page.php
+++ front-page.php
@@ -11,6 +11,7 @@ get_header(); ?>
     <?php endwhile; ?>
     </div>
     <div id="stories-right">
+        <h2 class="alt-group">Mekong Eye <br> News Digest</h2>
         <section class="sc-container" id="recent">
             <h2 class="alt">NewsStream</h2>
             <div class="sc-slice size-xs">
@@ -39,7 +40,10 @@ get_header(); ?>
                     }
                     ?>
                         <div class="sc-story__bd">
-                            <h4><?php echo $post->post_title; ?><i class="dateline"> <?php echo $author_first_name . ' ' . $author_last_name; ?></i></h4>
+                            <?php
+                            $kicker = wp_get_post_terms($post->ID, 'pub_type', array('fields' => 'names'));
+                            ?>
+                            <h4><?php echo ($kicker[0] == '' ? '' : '<b class="kicker">' . $kicker[0] . '</b> ');?><?php echo $post->post_title; ?><i class="dateline"> <?php echo $author_first_name . ' ' . $author_last_name; ?></i></h4>
                         </div>
                     </a>
                 </article>
@@ -49,23 +53,142 @@ get_header(); ?>
             </div>
         </section>
 
-        <section class="sc-container" id="">
-            <h2 class="alt">Press Releases</h2>
+        <section class="sc-container" id="resource">
+            <h2 class="alt">Resources & Press Releases</h2>
             <div class="sc-slice size-xs">
-                <article class="sc-story ">
-                    <a href="#">
+            <?php
+            $args = array(
+                'posts_per_page'   => 10,
+                'orderby'          => 'post_date',
+                'order'            => 'DESC',
+                'post_type'        => array('post', 'link', 'sequence', 'map'),
+                'post_status'      => 'publish',
+                'pub_type'         => 'Kicker, Eye Original, Opinion',
+                'suppress_filters' => true
+            );
+            $posts = get_posts( $args );
+            foreach ( $posts as $post ) {
+                $author_first_name = get_the_author_meta( 'first_name', $post->post_author );
+                $author_last_name = get_the_author_meta( 'last_name', $post->post_author );
+            ?>
+                <article class="sc-story">
+                    <?php 
+                    if ($post->post_type == 'link') {
+                        $link = get_post_meta($post->ID, 'link_target', true);
+                        echo '<a href="' . $link .'">';
+                    }
+                    else {
+                        echo '<a href="' . $post->guid .'">';
+                    }
+                    ?>
                         <div class="sc-story__bd">
-                            <h4>If there is no image, show only the text headine for the story <i class="dateline">25 May 2015</i></h4>
+                            <?php
+                            $kicker = wp_get_post_terms($post->ID, 'pub_type', array('fields' => 'names'));
+                            ?>
+                            <h4><?php echo ($kicker[0] == '' ? '' : '<b class="kicker">' . $kicker[0] . '</b> ');?><?php echo $post->post_title; ?><i class="dateline"> <?php echo $author_first_name . ' ' . $author_last_name; ?></i></h4>
                         </div>
                     </a>
                 </article>
+            <?php 
+            }
+            ?>
+            </div>
+        </section>
+
+        <section class="sc-container" id="editor">
+            <h2 class="alt">Editor’s Picks</h2>
+            <div class="sc-slice size-xs">
+            <?php
+            $args = array(
+                'posts_per_page'   => 10,
+                'orderby'          => 'post_date',
+                'order'            => 'DESC',
+                'post_type'        => array('post', 'link', 'sequence', 'map'),
+                'post_status'      => 'publish',
+                'meta_key'         => 'editor_pick',
+                'meta_value'       => "true",
+                'suppress_filters' => true
+            );
+            $posts = get_posts( $args );
+            foreach ( $posts as $post ) {
+                $author_first_name = get_the_author_meta( 'first_name', $post->post_author );
+                $author_last_name = get_the_author_meta( 'last_name', $post->post_author );
+            ?>
+                <article class="sc-story">
+                    <?php 
+                    if ($post->post_type == 'link') {
+                        $link = get_post_meta($post->ID, 'link_target', true);
+                        echo '<a href="' . $link .'">';
+                    }
+                    else {
+                        echo '<a href="' . $post->guid .'">';
+                    }
+                    ?>
+                        <div class="sc-story__bd">
+                            <?php
+                            $kicker = wp_get_post_terms($post->ID, 'pub_type', array('fields' => 'names'));
+                            ?>
+                            <h4><?php echo ($kicker[0] == '' ? '' : '<b class="kicker">' . $kicker[0] . '</b> ');?><?php echo $post->post_title; ?><i class="dateline"> <?php echo $author_first_name . ' ' . $author_last_name; ?></i></h4>
+                        </div>
+                    </a>
+                </article>
+            <?php 
+            }
+            ?>
+            </div>
+        </section>
+
+        <section class="sc-container" id="views">
+            <h2 class="alt">Most Read</h2>
+            <div class="sc-slice size-xs">
+            <?php
+            $args = array(
+                'posts_per_page'   => 10,
+                'orderby'          => 'meta_value',
+                'order'            => 'DESC',
+                'post_type'        => array('post', 'link', 'sequence', 'map'),
+                'post_status'      => 'publish',
+                'meta_key'         => 'post_views_count',
+                'suppress_filters' => true
+            );
+            $posts = get_posts( $args );
+            foreach ( $posts as $post ) {
+                $author_first_name = get_the_author_meta( 'first_name', $post->post_author );
+                $author_last_name = get_the_author_meta( 'last_name', $post->post_author );
+            ?>
+                <article class="sc-story">
+                    <?php 
+                    if ($post->post_type == 'link') {
+                        $link = get_post_meta($post->ID, 'link_target', true);
+                        echo '<a href="' . $link .'">';
+                    }
+                    else {
+                        echo '<a href="' . $post->guid .'">';
+                    }
+                    ?>
+                        <div class="sc-story__bd">
+                            <?php
+                            $kicker = wp_get_post_terms($post->ID, 'pub_type', array('fields' => 'names'));
+                            ?>
+                            <h4><?php echo ($kicker[0] == '' ? '' : '<b class="kicker">' . $kicker[0] . '</b> ');?><?php echo $post->post_title; ?><i class="dateline"> <?php echo $author_first_name . ' ' . $author_last_name; ?></i></h4>
+                        </div>
+                    </a>
+                </article>
+            <?php 
+            }
+            ?>
             </div>
         </section>
 
         <div id="subscribe-right">
-
-            <h3>Subscribe</h3>
-
+            <h3>Subscribe for email updates</h3>
+            <form class="email-signup form-color-e">
+                <label for="email-signup__name" class="hidden-label">Name</label>
+                <input type="email" value="" name="EMAIL" class="email" id="email-signup__name" placeholder="name" required="">
+                <label for="email-signup__mail" class="hidden-label">Email Address</label>
+                <input type="email" value="" name="EMAIL" class="email" id="email-signup__mail" placeholder="email address" required="">
+                <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button ss-icon">
+            </form>
         </div>
 
     </div>
