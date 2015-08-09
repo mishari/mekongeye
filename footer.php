a6c75a473784d45e14f286ab8499e3593c57f52e
BabyThor <w.wangtrakoon@gmail.com>
2015-08-09 10:35:12 +0700

    update footer follow mockup

--- footer.php
+++ footer.php
@@ -1,15 +1,62 @@
-<footer id="colophon">
-	<div class="container">
-		<div class="seven columns">
-			<nav id="footer-nav">
-				<?php wp_nav_menu(array('theme_location' => 'footer_menu')); ?>
-			</nav>
+<footer class="global-footer">
+	<div class="global-footer__bd">
+
+		<nav class="footer-directional">
+			<ul>
+				<li><a href="/">Home</a></li>
+				<li><a href="#nameplate">Top of Page</a></li>
+			</ul>
+		</nav>
+
+		<nav class="footer-utility">
+			<ul>
+				<li><a href="#">Contact Us</a></li>
+				<li><a href="#">Submit Content</a></li>
+				<li><a href="#">For Journalists</a></li>
+				<li><a href="#">Terms of Use</a></li>
+			</ul>
+		</nav>
+
+		<div class="footer-partners">
+			<h6>Partner Sites</h6>
+			<ul>
+				<li><a href="#">Mekong Matters Journalism Network</a></li>
+				<li><a href="#">Open Development Mekong</a></li>
+				<li><a href="#">Mekong Citizen</a></li>
+			</ul>
 		</div>
-		<div class="five columns">
-			<div class="credits">
-				<p><?php printf(__('This website is built on <a href="%s" target="_blank" rel="external">WordPress</a> using the <a href="%s" target="_blank" rel="external">JEO Beta</a> theme', 'jeo'), 'http://wordpress.org', 'http://jeo.cardume.art.br/'); ?></p>
-			</div>
+
+		<div class="footer-social">
+			<h6>Follow us on social media</h6>
+			<a href="#"><img src="/static/images/twitter-icon.png" alt="twitter"></a>
+			<a href="#"><img src="/static/images/facebook-icon.png" alt="facebook"></a>
 		</div>
+
+		<div class="footer-subscribe">
+			<h6>Subscribe for email updates</h6>
+			<form class="email-signup">
+				<label for="email-signup__name" class="hidden-label">Name</label>
+				<input type="email" value="" name="EMAIL" class="email" id="email-signup__name" placeholder="name" required="">
+				<label for="email-signup__mail" class="hidden-label">Email Address</label>
+				<input type="email" value="" name="EMAIL" class="email" id="email-signup__mail" placeholder="email address" required="">
+				<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button ss-icon">
+			</form>
+		</div>
+
+		<div class="footer-sponsors">
+			<h6>Project Sponsors</h6>
+			<p>
+				<a href="http://earthjournalism.net" style="width: 150px;"><img src="/static/images/ejn-logo-hi.png" alt="Earth Journalism Network"></a>
+			</p>
+		</div>
+
+		<div class="footer-contact">
+			<p>
+				<a href="#">editor@mekongeye.com</a><br>
+				Address goes here
+			</p>
+		</div>
+
 	</div>
 </footer>
 <?php wp_footer(); ?>
