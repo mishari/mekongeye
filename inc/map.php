2efa8fa103c4f4dcde5f05b281b2aa9eec1ac2a0
BabyThor <w.wangtrakoon@gmail.com>
2015-08-18 23:59:20 +0700

    Update map.php

--- inc/map.php
+++ inc/map.php
@@ -5,6 +5,11 @@
  * Map
  */
 
+function map_author_support()
+{
+    add_post_type_support('map', 'author');
+}
+
 function include_template_map( $template_path ) {
     if ( get_post_type() == 'map' ) {
         if ( is_single() ) {
@@ -16,5 +21,5 @@ function include_template_map( $template_path ) {
 }
 
 /* Add action to wp function */
-
 add_filter( 'template_include', 'include_template_map' );
+add_action('init', 'map_author_support', 100);
