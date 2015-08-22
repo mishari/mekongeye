f8120fe1a4aad9f38e5f06150fac333bcd066880
BabyThor <w.wangtrakoon@gmail.com>
2015-08-22 18:24:48 +0700

    Update map.php

--- inc/map.php
+++ inc/map.php
@@ -10,16 +10,5 @@ function map_author_support()
     add_post_type_support('map', 'author');
 }
 
-function include_template_map( $template_path ) {
-    if ( get_post_type() == 'map' ) {
-        if ( is_single() ) {
-            $template_path = get_stylesheet_directory() . '/content-map.php';
-        }
-    }
-
-    return $template_path;
-}
-
 /* Add action to wp function */
-add_filter( 'template_include', 'include_template_map' );
 add_action('init', 'map_author_support', 100);
