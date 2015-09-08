a44c425788a5340e580c27dfef7887dce95c872c
BabyThor <w.wangtrakoon@gmail.com>
2015-09-09 00:13:02 +0700

    Update content-page.php

--- content-page.php
+++ content-page.php
@@ -43,6 +43,7 @@ if ($pub_name != '' and $source_link != '') {
 <script type="text/javascript">jeo({"postID":<?php echo $map_id; ?>,"count":0});</script>
 <div class="main">
     <a name="content"></a>
+    <div id="stories-left">
     <div class="section-list">
 
         <header class="section-header">
@@ -140,6 +141,48 @@ if ($pub_name != '' and $source_link != '') {
             <?php } ?>
         </div>
 
+    </div>
+    </div>
+    <div id="stories-right">
+        <section class="sc-container" id="recent">
+            <h2 class="alt">NewsStream</h2>
+            <div class="sc-slice size-xs">
+            <?php 
+            $args = array(
+                'posts_per_page'   => 10,
+                'orderby'          => 'post_date',
+                'order'            => 'DESC',
+                'post_type'        => array('post', 'link', 'sequence', 'map'),
+                'post_status'      => 'publish',
+                'suppress_filters' => true
+            );
+            $posts = get_posts( $args );
+            foreach ( $posts as $post ) {
+            	$author_name = get_post_meta( $post->ID, 'author_name', true );
+            ?>
+                <article class="sc-story">
+                    <?php 
+                    if ($post->post_type == 'link') {
+                        $link = get_post_meta($post->ID, 'link_target', true);
+                        echo '<a href="' . $link .'">';
+                    }
+                    else {
+                        echo '<a href="' . post_permalink($post->ID) .'">';
+                    }
+                    ?>
+                        <div class="sc-story__bd">
+                            <?php
+                            $kicker = wp_get_post_terms($post->ID, 'pub_type', array('fields' => 'names'));
+                            ?>
+                            <h4><?php echo ($kicker[0] == '' ? '' : '<b class="kicker">' . $kicker[0] . '</b> ');?><?php echo $post->post_title; ?><i class="dateline"> <?php echo $author_name; ?></i></h4>
+                        </div>
+                    </a>
+                </article>
+            <?php 
+            }
+            ?>
+            </div>
+        </section>
     </div>
 
 </div>
