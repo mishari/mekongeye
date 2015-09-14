9fb309af89a39a959c8ac1801d23997498e81e0b
BabyThor <w.wangtrakoon@gmail.com>
2015-09-14 21:55:07 +0700

    Update yarpp-template-thumbnail.php

--- yarpp-template-thumbnail.php
+++ yarpp-template-thumbnail.php
@@ -1,1 +1,41 @@
-Test
+<?php
+/*
+YARPP Template: Thumbnails
+Description: Requires a theme which supports post thumbnails
+Author: mitcho (Michael Yoshitaka Erlewine)
+*/ ?>
+<section class="sc-container">
+
+    <h2>Related</h2>
+
+    <!-- begin slice -->
+    <div class="sc-slice size-md">
+        <?php if (have_posts()):?>
+                <?php while (have_posts()) : the_post(); ?>
+                <article class="sc-story option-image">
+                    <div class="sc-story__hd">
+                        <?php
+                            $image_src  = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
+                            if ($image_src != '') {
+                                $featured_image = '<img src="' . wpthumb( $image_src[0], $arg_large ) . '" alt="' . $post->post_title . '" />';
+                                echo $featured_image;
+                            }
+                        ?>
+                    </div>
+                    <a href="<?php echo post_permalink($post->ID) ?>">
+                        <div class="sc-story__bd">
+                            <h4><?php echo $post->post_title; ?></h4>
+                            <p class="dateline"><?php echo get_the_date( 'j M Y', $post->ID ); ?></p>
+                        </div>
+                    </a>
+                </article>
+                <?php endwhile; ?>
+        
+        <?php else: ?>
+        <p>No related photos.</p>
+        <?php endif; ?>
+
+    </div>
+    <!-- / slice -->
+
+</section>
