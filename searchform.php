7b3690997e5b9963809eb1d2a74ab969ecb3f68c
BabyThor <w.wangtrakoon@gmail.com>
2015-09-14 22:24:16 +0700

    Update searchform.php

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <div id="top-search">
-	<form role="search" method="get" class="search-form hidden" action="<?php echo esc_url( home_url( '/' ) ); ?>" style="display: block; opacity: 0;">
+	<form role="search" method="get" class="search-form hidden" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	    <input type="search" class="search-field" placeholder="Search …" value="" name="s" title="Search for:">
 	</form>
 </div>
