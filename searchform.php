ff191e2a3e83a97ac867acbc6464488895797df7
BabyThor <w.wangtrakoon@gmail.com>
2015-09-14 22:41:15 +0700

    Delete searchform.php

--- searchform.php
+++
@@ -1,3 +0,0 @@
-<form role="search" method="get" class="search-form hidden" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-<input type="search" class="search-field" placeholder="Search …" value="" name="s" title="Search for:">
-</form>
