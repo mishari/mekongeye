fbe4791ed93d590a1e89f60f0dd4e5ca125a9013
BabyThor <w.wangtrakoon@gmail.com>
2015-09-14 22:41:35 +0700

    Update header.php

--- header.php
+++ header.php
@@ -26,6 +26,16 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <script src="/wp-content/themes/mekongeye/assets/javascript/vendor/modernizr-2.8.3.min.js"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-67570914-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 
 
 <?php wp_head(); ?>
