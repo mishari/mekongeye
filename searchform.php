b2a7bef3907ab1cbbf6fe5ceecd342f5e8fcf05d
BabyThor <w.wangtrakoon@gmail.com>
2015-09-14 22:22:53 +0700

    Create searchform.php

---
+++ searchform.php
@@ -0,0 +1,5 @@
+<div id="top-search">
+	<form role="search" method="get" class="search-form hidden" action="<?php echo esc_url( home_url( '/' ) ); ?>" style="display: block; opacity: 0;">
+	    <input type="search" class="search-field" placeholder="Search …" value="" name="s" title="Search for:">
+	</form>
+</div>
