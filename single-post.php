ffe03adae50b6dd148bf63f43a0caede5596c1f7
BabyThor <w.wangtrakoon@gmail.com>
2015-09-14 18:56:07 +0700

    remove newstream section

--- single-post.php
+++ single-post.php
@@ -65,47 +65,6 @@
                 <?php the_content(); ?>
             </div>
             </div>
-            <div id="stories-right">
-        <section class="sc-container" id="recent">
-            <h2 class="alt">NewsStream</h2>
-            <div class="sc-slice size-xs">
-            <?php 
-            $args = array(
-                'posts_per_page'   => 10,
-                'orderby'          => 'post_date',
-                'order'            => 'DESC',
-                'post_type'        => array('post', 'link', 'sequence', 'map'),
-                'post_status'      => 'publish',
-                'suppress_filters' => true
-            );
-            $posts = get_posts( $args );
-            foreach ( $posts as $post ) {
-            	$author_name = get_post_meta( $post->ID, 'author_name', true );
-            ?>
-                <article class="sc-story">
-                    <?php 
-                    if ($post->post_type == 'link') {
-                        $link = get_post_meta($post->ID, 'link_target', true);
-                        echo '<a href="' . $link .'">';
-                    }
-                    else {
-                        echo '<a href="' . post_permalink($post->ID) .'">';
-                    }
-                    ?>
-                        <div class="sc-story__bd">
-                            <?php
-                            $kicker = wp_get_post_terms($post->ID, 'pub_type', array('fields' => 'names'));
-                            ?>
-                            <h4><?php echo ($kicker[0] == '' ? '' : '<b class="kicker">' . $kicker[0] . '</b> ');?><?php echo $post->post_title; ?><i class="dateline"> <?php echo $author_name; ?></i></h4>
-                        </div>
-                    </a>
-                </article>
-            <?php 
-            }
-            ?>
-            </div>
-        </section>
-    </div>
         </article>
         <section class="sc-container">
 
