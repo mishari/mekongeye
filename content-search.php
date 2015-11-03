aa2af3f1532d26b42d6b7390af69fad183584456
BabyThor <w.wangtrakoon@gmail.com>
2015-11-03 19:39:16 +0700

    Create content-search.php

---
+++ content-search.php
@@ -0,0 +1,39 @@
+<?php
+    global $post;
+?>
+<li class="post-list-item">
+    <article id="post-<?php echo $post->ID ?>">
+        <div class="post-list-thumbnail">
+            <?php
+                $link = get_post_meta($post->ID, 'link_target', true);
+                if ($link != '') {
+                    echo '<a href="' . $link .'" target="_blank">';
+                } else {
+                    echo '<a href="' . post_permalink($post->ID) .'">';
+                }
+                $thumbnail = get_the_post_thumbnail( $post->ID );
+                echo $thumbnail;
+            ?>
+        </div>
+        <div class="post-list-post-content">
+            <h2>
+                <?php
+                $link = get_post_meta($post->ID, 'link_target', true);
+                if ($link != '') {
+                    echo '<a href="' . $link .'" target="_blank">';
+                } else {
+                    echo '<a href="' . post_permalink($post->ID) .'">';
+                }
+                ?><?php echo $post->post_title ?></a>
+            </h2>
+            <?php
+                $date = get_post_meta( $post->ID, 'date', true);
+                if ($date == '') {
+                    $date = get_the_date( 'j M Y', $post->ID );
+                }
+            ?>
+            <p class="dateline"><?php echo $date ?></p>
+            <div class="excerpt"><p><?php echo $post->post_excerpt; ?></p></div>
+        </div>
+    </article>
+</li>
