c37a62c3b2933f52b18c3d8f169fefca1580acd8
BabyThor <w.wangtrakoon@gmail.com>
2015-11-12 22:26:22 +0700

    Update content-page.php

--- content-page.php
+++ content-page.php
@@ -163,24 +163,11 @@ $('#map-group li a').click(function() {
             <?php } ?>
         </div>
         <?php
-        $args = array(
-            'base'               => '%_%',
-            'format'             => '?page=%#%',
-            'total'              => 1,
-            'current'            => 0,
-            'show_all'           => False,
-            'end_size'           => 1,
-            'mid_size'           => 2,
-            'prev_next'          => True,
-            'prev_text'          => __('« Previous'),
-            'next_text'          => __('Next »'),
-            'type'               => 'plain',
-            'add_args'           => False,
-            'add_fragment'       => '',
-            'before_page_number' => '',
-            'after_page_number'  => ''
-        );
-        the_posts_pagination( $args );
+        the_posts_pagination( array(
+        	'mid_size'  => 2,
+        	'prev_text' => __( 'Back', 'textdomain' ),
+        	'next_text' => __( 'Onward', 'textdomain' ),
+        ) );
         ?>
 
     </div>
